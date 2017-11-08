24be6bdec0073e22704a4409f9f73a2a7d66bdcf
alfablac <likao89@hotmail.com>
2017-11-08 13:52:47 -0200

    Add files via upload
    Initial login using WinForms

---
+++ FacebookScraper/FacebookScraper/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace FacebookScraper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtlogin = new System.Windows.Forms.TextBox();
+            this.txtsenha = new System.Windows.Forms.TextBox();
+            this.btnLog = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Senha";
+            // 
+            // txtlogin
+            // 
+            this.txtlogin.Location = new System.Drawing.Point(161, 47);
+            this.txtlogin.Name = "txtlogin";
+            this.txtlogin.Size = new System.Drawing.Size(155, 20);
+            this.txtlogin.TabIndex = 2;
+            // 
+            // txtsenha
+            // 
+            this.txtsenha.Location = new System.Drawing.Point(161, 95);
+            this.txtsenha.Name = "txtsenha";
+            this.txtsenha.PasswordChar = '*';
+            this.txtsenha.Size = new System.Drawing.Size(155, 20);
+            this.txtsenha.TabIndex = 3;
+            this.txtsenha.UseSystemPasswordChar = true;
+            // 
+            // btnLog
+            // 
+            this.btnLog.Location = new System.Drawing.Point(152, 150);
+            this.btnLog.Name = "btnLog";
+            this.btnLog.Size = new System.Drawing.Size(75, 23);
+            this.btnLog.TabIndex = 4;
+            this.btnLog.Text = "Logar";
+            this.btnLog.UseVisualStyleBackColor = true;
+            this.btnLog.Click += new System.EventHandler(this.btnLog_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 413);
+            this.Controls.Add(this.btnLog);
+            this.Controls.Add(this.txtsenha);
+            this.Controls.Add(this.txtlogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtlogin;
+        private System.Windows.Forms.TextBox txtsenha;
+        private System.Windows.Forms.Button btnLog;
+    }
+}
+
---
+++ FacebookScraper/FacebookScraper/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+
+namespace FacebookScraper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLog_Click(object sender, EventArgs e)
+        {
+
+            IWebDriver driver = new FirefoxDriver("C:\\Program Files");
+            driver.Manage().Window.Position.X.Equals(0);
+            driver.Manage().Window.Position.Y.Equals(0);
+            driver.Url = "https://www.facebook.com";
+            driver.FindElement(By.Id("email")).SendKeys(txtlogin.Text);
+            driver.FindElement(By.Id("pass")).SendKeys(txtsenha.Text);
+            driver.FindElement(By.Id("loginbutton")).Click();
+
+        }
+    }
+}
